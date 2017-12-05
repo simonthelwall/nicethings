8eb35e78fd23793c6ce211591a482e5dbfb94637
Simon Thelwall <simon.thelwall@gmail.com>
2017-12-05 12:00:18 +0000

    Added standard error of difference in means and confidence interval for difference in means

--- R/nice_diff_means.R
+++ R/nice_diff_means.R
@@ -19,10 +19,6 @@ nice_diff_means <- function(data, var, by){ # , ci = TRUE
                           msg = "There can only be two elements in the grouping variable")
   assertthat::assert_that(is.numeric(data[[var]]) == TRUE ,
                            msg = "var must be numeric")
-  # stopifnot(length(unique(data[[by]])) == 2,
-  #           error = stop("There can only be two elements in the grouping variable"))
-  # stopifnot(is.numeric(data[[var]]) == TRUE,
-  #           error = stop(paste0(data, "$", var, " must be numeric")))
 
   by_1 <- unique(data[[by]])[1]
   by_2 <- unique(data[[by]])[2]
@@ -33,5 +29,90 @@ nice_diff_means <- function(data, var, by){ # , ci = TRUE
   diff_in_means <- mean_1 - mean_2
   # need nice rounding
   return(diff_in_means)
-  # add ci too.
+  # add ci too. # need se function to do this
+}
+
+#' Standard error for difference in means
+#'
+#' Calculates the standard error for the difference in means between two groups
+#'
+#' @param data A data frame
+#' @param var A numeric variable containing the data for the calculation of the means
+#' @param by A grouping variable with only two values
+#' @return A numeric value for the difference in means
+#'
+#' @examples
+#' data(sleep)
+#' sleep$group <- as.numeric(sleep$group)
+#' se_diff_means(data = sleep, var = "extra", by = "group")
+#'
+#' # confirming against Kirkwood and Sterne
+#' dat <- data.frame(weight = c(3.18, 2.74, 2.90, 3.27, 3.65, 3.42, 3.23, 2.86,
+#'                                3.6, 3.65, 3.69, 3.53, 2.38, 2.34, 3.99, 3.89,
+#'                                3.60, 3.73, 3.31, 3.7, 4.08, 3.61, 3.83, 3.41,
+#'                                4.13, 3.36, 3.54, 3.51, 2.71),
+#'                     group = c(rep(1, 14), rep(0, 15)))
+#' se_diff_means(data = dat, var = "weight", by = "group")
+#' @export
+
+se_diff_means <- function(data, var, by){
+  # sqrt(sd_mean1 + sd_mean2)
+
+  # check for two levels of `by`, then stop if not 2
+  assertthat::assert_that(length(unique(data[[by]])) == 2,
+                          msg = "There can only be two elements in the grouping variable")
+  assertthat::assert_that(is.numeric(data[[var]]) == TRUE ,
+                          msg = "var must be numeric")
+
+  by_1 <- unique(data[[by]])[1]
+  by_2 <- unique(data[[by]])[2]
+
+  sd_1 <- stats::sd(data[[var]][ data[[by]] == by_1 ], na.rm = TRUE)
+  sd_2 <- stats::sd(data[[var]][ data[[by]] == by_2 ], na.rm = TRUE)
+
+  step1_1 <- (sd_1^2)/length(data[[by]][data[[by]] == by_1])
+  step1_2 <- (sd_2^2)/length(data[[by]][data[[by]] == by_2])
+
+  se_diff <- sqrt(( step1_1 + step1_2 ))
+  return(se_diff)
+}
+
+#' Confidence interval for difference in means
+#'
+#' Calulates a 95% CI for the difference between two means.
+#' Uses an approximation to degrees of freedom of n observations - 2.
+#' Thus, may give broader confidence intervals than built in \code{t.test()} function
+#'
+#' @param data A data frame
+#' @param var A numeric variable containing the data for the calculation of the means
+#' @param by A grouping variable with only two values
+#' @return A numeric value for the difference in means
+#'
+#' @examples
+#' data(sleep)
+#' sleep$group <- as.numeric(sleep$group)
+#' ci_diff_means(data = sleep, var = "extra", by = "group")
+#' @export
+
+ci_diff_means <- function(data, var, by){
+
+  # check for two levels of `by`, then stop if not 2
+  assertthat::assert_that(length(unique(data[[by]])) == 2,
+                          msg = "There can only be two elements in the grouping variable")
+  assertthat::assert_that(is.numeric(data[[var]]) == TRUE ,
+                          msg = "var must be numeric")
+
+  # proof that quantile function is what I want:
+  # qnorm(0.025) == 1.96 (critical value for 5% two-tailed normal distribution)
+  # also qt(0.025, 27) matches t' given p 65
+  # qt is quantile function of t distribution
+  # requires p = vector of probabilities
+  #     df = degrees of freedom
+  se <- se_diff_means(data, var, by)
+  estimate <- nice_diff_means(data, var, by)
+  t_crit <- abs(stats::qt(0.025, df = length(data[[by]]) - 2 ))
+  lci <- estimate - (t_crit * se)
+  uci <- estimate + (t_crit * se)
+  z <- paste0("(", sprintf(lci, fmt = "%0.2f"), "-", sprintf(uci, fmt = "%0.2f"), ")")
+  return(z)
 }
--- tests/testthat/test-diff_means.R
+++ tests/testthat/test-diff_means.R
@@ -1,18 +1,66 @@
 context("Difference in means")
 
-test_that("Function stops if length(unique(by)) != 2", {
+test_that("nice_diff_means stops if length(unique(by)) != 2", {
   dat <- data.frame(x = c(1:5), y = rep(1, 5))
   expect_error(nice_diff_means(data = dat, var = "x", by = "y"))
   dat <- data.frame(x = c(1:6), y = c(1,1,2,2,3,3))
   expect_error(nice_diff_means(data = dat, var = "x", by = "y"))
 })
 
-test_that("Function stops if var is not numeric", {
+test_that("nice_diff_means stops if var is not numeric", {
   dat <- data.frame(x = as.character(c(1:6)), y = c(1,1,1,2,2,2))
   expect_error(nice_diff_means(data = dat, var = "x", by = "y"))
 })
 
-test_that("Function works on numeric data, with two groups", {
+test_that("nice_diff_means works on numeric data, with two groups", {
   dat <- data.frame(x = rep(3, 6), y = c(1,1,1,2,2,2))
   expect_equal(nice_diff_means(data = dat, var = "x", by = "y"), 0)
 })
+
+test_that("nice_diff_means returns something close to the built in t.test function", {
+  data(sleep)
+  sleep$group <- as.numeric(sleep$group)
+  expect_equal(abs(nice_diff_means(data = sleep, var = "extra", by = "group")),
+               abs(t.test(sleep$extra[sleep$group == 1], sleep$extra[sleep$group == 2])$estimate[[1]] -
+                     t.test(sleep$extra[sleep$group == 1], sleep$extra[sleep$group == 2])$estimate[[2]] )
+  )
+})
+
+# se_diff_means ####
+
+test_that("se_diff_means stops if length(unique(by)) != 2", {
+  dat <- data.frame(x = c(1:5), y = rep(1, 5))
+  expect_error(se_diff_means(data = dat, var = "x", by = "y"))
+  dat <- data.frame(x = c(1:6), y = c(1,1,2,2,3,3))
+  expect_error(se_diff_means(data = dat, var = "x", by = "y"))
+})
+
+test_that("se_diff_means stops if var is not numeric", {
+  dat <- data.frame(x = as.character(c(1:6)), y = c(1,1,1,2,2,2))
+  expect_error(se_diff_means(data = dat, var = "x", by = "y"))
+})
+
+test_that("se_diff_means works on numeric data, with two groups", {
+  dat <- data.frame(x = rep(3, 6), y = c(1,1,1,2,2,2))
+  expect_equal(se_diff_means(data = dat, var = "x", by = "y"), 0)
+})
+
+# ci_diff_means ####
+
+test_that("ci_diff_means returns result equivalent to Kirkwood and Sterne", {
+  dat <- data.frame(weight = c(3.18, 2.74, 2.90, 3.27, 3.65, 3.42, 3.23, 2.86,
+                               3.6, 3.65, 3.69, 3.53, 2.38, 2.34, 3.99, 3.89,
+                               3.60, 3.73, 3.31, 3.7, 4.08, 3.61, 3.83, 3.41,
+                               4.13, 3.36, 3.54, 3.51, 2.71),
+                    group = c(rep(1, 14), rep(0, 15)))
+  expect_equal(ci_diff_means(data = dat, var = "weight", by = "group"),
+               "(-0.77--0.14)")
+})
+
+# Could do this within 10% by substr and as.numeric.
+# for a later date.
+# test_that("ci_diff_means returns something similar to built in t.test", {
+#   data(sleep)
+#   expect_equal(ci_diff_means(data = sleep, var = "extra", by = "group"),
+#                "(-3.37-0.21)")
+# })
