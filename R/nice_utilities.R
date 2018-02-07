1ccdb5c789098475a17db9585300fad3ceada38e
Simon Thelwall <simon.thelwall@phe.gov.uk>
2018-02-07 14:18:25 +0000

    Fixed nice_load so that it works as expected.
    Reworked nice_load as per SO post on loading into rdb or rdx.
    Fixes issue #6
    Includes rename option to allow renaming.

--- R/nice_utilities.R
+++ R/nice_utilities.R
@@ -65,8 +65,9 @@ pipeable_nice_names <- function(x){
 #' However, one may wish to load only a single object, if, for example, one has conflicting object names in the same environment.
 #' This function also allows the user to rename an object within the data frame.
 #' Unfortunately, it is not possible to selectively load objects so this function loads every thing in the data file, and then drops what is not required.
+#' Cribbed from https://stackoverflow.com/questions/8700619/get-specific-object-from-rdata-file
 #'
-#' @param file An RData file
+#' @param file An .RData file
 #' @param object An object known to be saved within the RData file
 #' @param rename String, a new name for the object to take
 #'
@@ -76,24 +77,42 @@ pipeable_nice_names <- function(x){
 #' save(x,y, file = paste0(tempdir(), "/temp.RData"))
 #' rm(x, y)
 #' nice_load(file = paste0(tempdir(), "/temp.RData"), "y")
+#' nice_load(file = paste0(tempdir(), "/temp.RData"), "y", rename = "z")
 #' @export
 
 nice_load <- function(file, object, rename = NULL){
-  load(file)
 
-  # drop everything that is not the desired object and is not rename
-  rm(list = ls()[!(ls() %in% c(object, rename) )])
-  # I think rename gets dropped here and therefore subsequent lines can't run
-  # but there's not a lot of point trying to fix this, as it is not a good solution anyway
+  if (!requireNamespace("stringr", quietly = TRUE)) {
+    stop("stringr needed for this function to work. Please install it.",
+         call. = FALSE)
+  }
 
+  # assertthat::assert_that(is.character(file), "file must be a string")
+  # assertthat::assert_that(is.character(object), "object must be a string")
+  # assertthat::assert_that((is.character(rename) | is.null(rename)), "rename must be a string or NULL")
+
+  file_string <- stringr::str_replace(file, "^.*/", "")
+  file_string <- stringr::str_replace(file, "\\.RData", "")
+
+  # get data frame into local environment
+  e = local({load(file); environment()})
+
+  # make lazy-load database
+  tools:::makeLazyLoadDB(e, file_string)
+  lazyLoad(file_string)
+
+  # load object
+  get(object)
 
   if(!is.null(rename) ){
     # create object in local env that has name matching value for object, with new name same as rename
-    assign(ls()[ls() == eval(object)], rename)
-    return(get(eval(quote(rename))))
+    assign(eval(rename), get(object), envir = .GlobalEnv)
+    # assign(ls()[ls() == eval(object)], rename)
+    rm(e)
+    # return(get(eval(quote(rename))))
   }
   else{
-    # return what is left, that is not rename
-    return(get(ls()))
+    rm(e)
+    assign(eval(object), get(object), envir = .GlobalEnv)
   }
 }
--- tests/testthat/test-utilities.R
+++ tests/testthat/test-utilities.R
@@ -7,4 +7,8 @@ test_that("nice_load works as expected", {
   rm(x, y)
   nice_load(file = paste0(tempdir(), "/temp.RData"), "y")
   expect_true(exists("y"))
+  nice_load(file = paste0(tempdir(), "/temp.RData"), "y", rename = "z")
+  expect_true(exists("z"))
+  expect_equal(z, 2)
+  expect_equal(y, 2)
 })
