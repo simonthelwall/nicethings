732283a4de5f35d33baedb159da72531aab24a44
Simon Thelwall <simon.thelwall@gmail.com>
2018-06-03 11:55:10 +0100

    Added tests for rate_confint. updated pkgdown site

--- tests/testthat/test-rate.R
+++ tests/testthat/test-rate.R
@@ -10,8 +10,16 @@ test_that("Incorrect inputs are caught", {
   expect_error(rate_confint(1, -10), "Numerator and denominator must be positive")
 })
 
+test_that("rate_confint returns expected outputs", {
+  expect_true(is.data.frame(rate_confint(5, 25)))
+  expect_equal(rate_confint(5, 25)$rate, 0.2, tolerance = .002)
+  expect_equal(rate_confint(5, 25)$lci, 0.0832, tolerance = .002)
+  expect_equal(rate_confint(5, 25)$uci, 0.4805, tolerance = .002)
+})
+
 test_that("Rate difference output returns expected value from Kirkwood and Sterne, p241",{
   expect_equal(rate_difference(33, 355, 24, 518)$rd, 0.0466257)
   expect_equal(rate_difference(33, 355, 24, 518)$lci, 0.009889584)
   expect_equal(rate_difference(33, 355, 24, 518)$uci, 0.08336182)
 })
+
